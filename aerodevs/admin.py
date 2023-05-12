3cf1c9c76b526c8f78067d1a41fb31a6a6c7c9f8
Babithakumari <babithakumari120@gmail.com>
2023-05-12 11:57:36 +0530

    views created

--- aerodevs/admin.py
+++ aerodevs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+
+
+
+class UserAdmin(admin.ModelAdmin):
+    user_display = ["username","password","email","role"]
+
+admin.site.register(User, UserAdmin)
+
--- aerodevs/models.py
+++ aerodevs/models.py
@@ -2,5 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+    
+
 class User(AbstractUser):
-    pass
+    LOGIN_TYPE = [
+        ("R", "Recycler"),
+        ("M", "Manufacturer"),
+        ("A", "Airline"),
+    ]
+    role = models.CharField(max_length=1,choices = LOGIN_TYPE,null=True)
